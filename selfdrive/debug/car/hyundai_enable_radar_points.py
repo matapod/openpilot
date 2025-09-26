3bfacc516a6ab3429e2a03b36f8743e45a2ac004
matapod <matapods@naver.com>
2025-09-26 09:04:47 +0900

    dhpilot Service - selfdrive/debug/car/hyundai_enable_radar_points.py

--- selfdrive/debug/car/hyundai_enable_radar_points.py
+++ selfdrive/debug/car/hyundai_enable_radar_points.py
@@ -52,6 +52,10 @@ SUPPORTED_FW_VERSIONS = {
   b"LX2_ SCC FHCUP      1.00 1.00 99110-S8110!\x04\x05\x17\x01    ": ConfigValues(
     default_config=b"\x00\x00\x00\x01\x00\x00",
     tracks_enabled=b"\x00\x00\x00\x01\x00\x01"),
+  # 2023 SANTA FE TM FL
+  b"TM__ SCC FHCUP      1.00 1.00 99110-S1500 \x04!\x15\x07    ": ConfigValues(
+    default_config=b"\x00\x00\x00\x01\x00\x00",
+    tracks_enabled=b"\x00\x00\x00\x01\x00\x01"),
   # 2020 SANTA FE
   b"TM__ SCC F-CUP      1.00 1.03 99110-S2000\x19\x050\x13'    ": ConfigValues(
     default_config=b"\x00\x00\x00\x01\x00\x00",
